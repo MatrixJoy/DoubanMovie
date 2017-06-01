68655d99aa25ae2d730f85f60bc0c946252d9570
zhoujunjiang <785375121@qq.com>
2017-06-01 15:51:14 +0800

    finish project

---
+++ app/src/main/java/com/aidchow/doubanmovie/searchmovie/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.aidchow.doubanmovie.searchmovie
+
+/**
+ * Created by aidchow on 17-6-1.
+ */
+class SearchViewModel {
+}
---
+++ app/src/main/java/com/aidchow/doubanmovie/ui/AboutActivity.kt
@@ -0,0 +1,7 @@
+package com.aidchow.doubanmovie.ui
+
+/**
+ * Created by aidchow on 17-6-1.
+ */
+class AboutActivity {
+}
